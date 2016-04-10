8114d522a026d69da3b9fd9c08bf69bd7c13d2f3
narasimha14 <narasimha.appdev@gmail.com>
2016-04-10 19:03:34 -0400

    First commit

---
+++ game_development/multisprite.d
@@ -0,0 +1,2 @@
+multisprite.o multisprite.d : multisprite.cpp multisprite.h drawable.h vector2f.h \
+ frame.h gamedata.h parseXML.h
---
+++ game_development/twowaymultisprite.d
@@ -0,0 +1,2 @@
+twowaymultisprite.o twowaymultisprite.d : twowaymultisprite.cpp twowaymultisprite.h drawable.h \
+ vector2f.h frame.h gamedata.h parseXML.h
